84132dfd7a941cf491bba1052951a4bc28cf424d
LuckDev <lukedru@yahoo.com>
2023-04-17 23:32:07 -0700

    Initial Commit

---
+++ Gojo/GojoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GojoApp.swift
+//  Gojo
+//
+//  Created by Luke Drushell on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GojoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
